7aa6371a9cad14ce3a017a5b0a67413a7789ad34
JTrovs16 <jessica.trovarello@hotmail.com>
2025-06-23 23:00:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function writeOwn() {
+  alert('Manual input enabled. Start typing in the box.');
+}
+
+function generateAI() {
+  alert('AI-generated text placeholder. Integration coming soon.');
+}
